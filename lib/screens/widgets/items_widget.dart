a9db9094673c1bc2c21cd766526073d5a05daeae
Yeison Hernandez <developer.ss.2@tucash.co>
2023-09-11 08:02:50 -0500

    Maquetación del home

---
+++ lib/screens/widgets/items_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ItemsWidget extends StatelessWidget {
+  const ItemsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
